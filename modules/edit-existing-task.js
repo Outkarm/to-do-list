e618710c04fe63995dcd2d37d01ef4e8ff056c35
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-10-14 20:25:33 +0000

    add test for utilities

--- modules/edit-existing-task.js
+++ modules/edit-existing-task.js
@@ -1,19 +1,7 @@
-import allTask from './tasks-array-module.js';
-import localStorageUpdate from './dynamic-storage-update.js';
+/* eslint-disable */
+import allTask from "./tasks-array-module.js";
+import localStorageUpdate from "./dynamic-storage-update.js";
 
-const editTask = (obj, indx) => {
-  obj.contentEditable = 'true';
-  obj.onkeydown = (event) => {
-    if (event.key === 'Enter') {
-      obj.contentEditable = 'false';
-      allTask.forEach((e) => {
-        if (e.index === Number(indx)) {
-          e.description = obj.innerText;
-        }
-      });
-      localStorageUpdate(allTask);
-    }
-  };
-};
+import editTask from "../_mock_/utilities.js";
 
 export default editTask;
